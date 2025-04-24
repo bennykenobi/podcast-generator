4c5b30cc0b9c60fb7ff9f0b9e8ab5cf8d91c2a74
bennykenobi <manolas.b@gmail.com>
2025-04-24 06:17:13 +0000

    try this

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,19 @@
 FROM ubuntu:latest
 
 RUN apt-get update && apt-get install -y \
-  python3.10 \
+  python3 \
   python3-pip \
+  python3-venv \
   git
+
+RUN python3 -m venv /venv
+ENV PATH="/venv/bin:$PATH"
 
 RUN pip install pyYAML
 
 COPY feed.py /user/bin/feed.py
-
 COPY entrypoint.sh /entrypoint.sh
+
+RUN chmod +x /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-echo "==============="
+set -e
+
+if [ ! -f "feed.yaml"]; then
+    echo "Error: feed.yaml not found in the current directory"
+    exit 1
+fi
 
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUTEMAIL}"
@@ -11,4 +16,4 @@ python3 /usr/bin/feed.py
 git add -A && git commit -m "Update Feed"
 git push --set-upstream origin main
 
-echo "==============="
+echo "RSS feed generated successfully"
